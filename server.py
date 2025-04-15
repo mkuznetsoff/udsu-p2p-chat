ff2e54d876d29dcb4faf29161347e9b651a798e4
mkuznetsoff <mk0805208@gmail.com>
2025-04-15 12:59:34 +0400

    beta

--- server.py
+++ server.py
@@ -1,36 +1,41 @@
-# p2p_key_server.py
 import socket
 
 UDP_MAX_SIZE = 65535
 
-def listen(host: str = '127.0.0.1', port: int = 3000):
+def listen(host: str = '0.0.0.0', port: int = 3000):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.bind((host, port))
     print(f'Listening at {host}:{port}')
 
-    members = {}  # addr -> public_key
-
+    members = {}
+    
     while True:
         msg, addr = s.recvfrom(UDP_MAX_SIZE)
 
+        if addr not in members:
+            try:
+                members[addr] = msg
+                print(f'Client {addr} joined and sent public key')
+                # Рассылаем новый ключ всем участникам
+                for member in members:
+                    if member != addr:
+                        s.sendto(msg, member)
+                continue
+            except:
+                continue
+        
         if msg == b'__request_keys':
-            # Клиент запросил ключи всех других участников
+            # Отправляем запрошенные ключи
             for member, key in members.items():
                 if member != addr:
                     s.sendto(key, addr)
             continue
-
-        # Предполагаем, что это публичный ключ нового клиента
-        if addr not in members:
-            members[addr] = msg
-            print(f'[+] Зарегистрирован клиент {addr}, отправлен публичный ключ')
-
-            # Рассылаем его ключ другим клиентам
-            for member in members:
-                if member != addr:
-                    s.sendto(msg, member)
-        else:
-            print(f'[!] Неизвестное сообщение от {addr}, проигнорировано')
+        
+        print(f'Relaying encrypted message from {addr}')
+        for member in members:
+            if member == addr:
+                continue
+            s.sendto(msg, member)
 
 if __name__ == '__main__':
     listen()
